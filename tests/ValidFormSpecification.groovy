5fb88c4c32d822caf689880b866666254c6bfb74
Dennis Weber <dennis.weber@seitenbau.com>
2023-12-04 11:53:35 +0100

    Add running unit tests

--- tests/ValidFormSpecification.groovy
+++ tests/ValidFormSpecification.groovy
@@ -4,10 +4,11 @@ import spock.lang.Specification
 class ValidFormSpecification extends Specification {
   def "Verify form is valid"() {
     when:
-    // TODO: Add the filenames of all forms in this project here.
-    //   e.g. List<String> forms = ["Staatskanzlei_OpferVonGewalttaten_Applicant-v1.0-de.json"]
-    throw new RuntimeException("Did you forget to add all the forms to the ValidFormSpecification?")
-    List<String> forms = []
+    List<String> forms = [
+            "CustomerName_ProcessName_ApplicantForm-v1.0-de.json", // TODO: Update the file names
+            "CustomerName_ProcessName_PreliminaryForm-v1.0-de.json",
+            "CustomerName_ProcessName_SummaryForm-v1.0-de.json",
+    ]
     forms.each { form ->
       new FormValidator(getClass().getResourceAsStream(form).text).validate()
     }
