914ae2da5d98dcd0253c629a815ebca0d66f4a5b
Dennis Weber <dennis.weber@seitenbau.com>
2024-01-25 09:42:24 +0100

    Configure summary form

--- tests/ValidFormSpecification.groovy
+++ tests/ValidFormSpecification.groovy
@@ -7,7 +7,7 @@ class ValidFormSpecification extends Specification {
     List<String> forms = [
             "govTecEventSubmitForm_ApplicantForm-v1.0-de.json", // TODO: Update the file names
             "CustomerName_ProcessName_PreliminaryForm-v1.0-de.json",
-            "CustomerName_ProcessName_SummaryForm-v1.0-de.json",
+            "govTecEventSubmitForm_SummaryForm-v1.0-de.json",
     ]
     forms.each { form ->
       new FormValidator(getClass().getResourceAsStream(form).text).validate()
