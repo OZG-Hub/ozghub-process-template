6f8373e6c1761abbee5c84c5e45692985ac96a57
Piet Schumacher <piet.schumacher@seitenbau.com>
2026-04-29 10:37:00 +0200

    Add zsk task

--- scripts/processIdHere/init.groovy
+++ scripts/processIdHere/init.groovy
@@ -7,3 +7,12 @@ ScriptingApiV1 api = apiV1 // Variable is automatically set by Serviceportal pro
 // Allow users to stop/delete the currently running process instance
 // See https://doku.pmp.seitenbau.com/pages/viewpage.action?pageId=394836
 api.setVariable("killable", true)
+
+String leistungsschluessel
+try {
+  leistungsschluessel = api.startParameter.leistung.id
+} catch (NullPointerException ne) {
+  api.logger.warn("Unable to determine the leika key; using null instead.", ne)
+  leistungsschluessel = null
+}
+api.setVariable("leistungsschluessel", leistungsschluessel)
